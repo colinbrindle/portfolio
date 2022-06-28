5cf0c6c850dbb7817a46021ad3c12cbb5e19db1b
colin brindle <hcolinbrindle@gmail.com>
2022-06-28 18:36:28 -0400

    Minor clean up

--- src/App/App.js
+++ src/App/App.js
@@ -12,10 +12,10 @@ function App() {
     <Container fluid className="vh-100 d-flex flex-column g-0">
       <Nav />
       <Row className="g-0">
-        <Col s={12} md={4} className="col-spacing">
+        <Col s={12} md={4} className="">
           <Bio />
         </Col>
-        <Col s={12} md={8} className="bg-danger col-spacing">
+        <Col s={12} md={8} className="bg-danger">
           <Portfolio />
         </Col>
       </Row>
