7d761e3a3c91a5ab60b5c16d15648cfae79b782a
colin brindle <hcolinbrindle@gmail.com>
2022-07-04 19:08:03 -0400

    Additional updates to portfolio section -- finally getting images to size correctly

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,55 +1,70 @@
 import React from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Image, Col, Card } from "react-bootstrap";
+import { Container, Row, Image, Col } from "react-bootstrap";
 import "./Portfolio.css";
 
 import unn from "./imgs/unn.png";
 import tiw from "./imgs/thisiswar.png";
 import tiw2 from "./imgs/thisiswar2.png";
 import mossii from "./imgs/drmossii.png";
+import placeholderImg from "./imgs/placeholder.png";
 
 function Portfolio() {
   return (
     <Container fluid className="port-container">
       <Row>
-        <div className="pedestal">
-          <Image
-            fluid
-            src={tiw2}
-            className="item-fit"
-            alt="Image of This is War 2 landing page"
-          ></Image>
-        </div>
+        <Col s={12}>
+          <div className="pedestal">
+            <Image
+              fluid
+              src={tiw2}
+              alt="Image of This is War 2 landing page"
+            ></Image>
+          </div>
+        </Col>
       </Row>
-      <Row>
-        <div className="item">
-          <Image
-            fluid
-            src={tiw}
-            className="item-fit"
-            alt="Image of This is War landing page"
-          ></Image>
-        </div>
-        <div className="item">
-          <Image
-            fluid
-            src={unn}
-            className="item-fit"
-            alt="Image of Ukraine News Now news page"
-          ></Image>
-        </div>
-        <div className="item">
-          <Image
-            fluid
-            src={mossii}
-            className="item-fit"
-            alt="Still from Dr Mossii promotional video"
-          ></Image>
-        </div>
-        <div className="item">
-          <h1 className="placeholder-item">?</h1>
-        </div>
+      <Row className="g-0">
+        <Col s={12} xl={6}>
+          <div className="item-box">
+            <Image
+              fluid
+              src={tiw}
+              alt="Image of This is War landing page"
+              className="image-sizing"
+            ></Image>
+          </div>
+        </Col>
+        <Col s={12} xl={6}>
+          <div className="item-box">
+            <Image
+              fluid
+              src={unn}
+              alt="Image of Ukraine News Now news page"
+              className="image-sizing"
+            ></Image>
+          </div>
+        </Col>
+        <Col s={12} xl={6}>
+          <div className="item-box">
+            <Image
+              fluid
+              src={mossii}
+              alt="Still from Dr Mossii promotional video"
+              className="image-sizing"
+            ></Image>
+          </div>
+        </Col>
+        <Col s={12} xl={6}>
+          <div className="item-box">
+            <Image
+              fluid
+              src={placeholderImg}
+              alt="Placeholder image for new projects"
+              className="image-sizing"
+            ></Image>
+          </div>
+        </Col>
       </Row>
     </Container>
   );
